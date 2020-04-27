6f47686cffd589e95c4358dca08e14298d2e2bb8
Takeshi Takatsudo <takazudo@gmail.com>
2020-04-27 15:53:56 +0900

    bump

--- functions/tweet-to-trello/tweet-to-trello.js
+++ functions/tweet-to-trello/tweet-to-trello.js
@@ -44,6 +44,7 @@ const sendExtraRequest = () => {
 
 const sendFetchPageText = async (cardId, path) => {
   const url = `${process.env.URL}${path}`
+  console.log('sendFetchPageTo:', url)
   return fetch(url, {
     method: "POST",
     headers: {
