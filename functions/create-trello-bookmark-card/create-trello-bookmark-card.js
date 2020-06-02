7f82126cab47d7e94428861cd62f14c0b75a6eea
Takeshi Takatsudo <takazudo@gmail.com>
2020-06-03 03:26:10 +0900

    create-trello-card

--- functions/create-trello-bookmark-card/create-trello-bookmark-card.js
+++ functions/create-trello-bookmark-card/create-trello-bookmark-card.js
@@ -1,8 +1,40 @@
+const fetch = require("node-fetch");
+
 const raiseError = (message) => {
   console.error(message);
 };
 
-exports.handler = (event) => {
+const createParams = ({ url, idList }) => {
+  const params = new URLSearchParams();
+  const { TRELLO_API_KEY: key, TRELLO_API_TOKEN: token } = process.env;
+  params.append("pos", "top");
+  params.append("idList", idList);
+  params.append("key", key);
+  params.append("token", token);
+  params.append("urlSource", url);
+  return params;
+};
+
+const createCard = async (params) => {
+  const response = await fetch("https://api.trello.com/1/cards", {
+    method: "post",
+    headers: { Accept: "application/json" },
+    body: params,
+  });
+  return response;
+};
+
+// get targetList from env value.
+// need to define list id in .env file.
+const getTargetListId = (targetList) => {
+  const idList = process.env[`TARGET_LIST_${targetList}`];
+  if (!idList) {
+    throw new Error(`targetList not defined: ${targetList}`);
+  }
+  return idList;
+};
+
+exports.handler = async (event) => {
   // check method
   if (event.httpMethod !== "POST") {
     raiseError("ERR: method is not post");
@@ -22,7 +54,7 @@ exports.handler = (event) => {
     };
   }
 
-  const { url } = JSON.parse(event.body);
+  const { url, targetList } = JSON.parse(event.body);
 
   // check params
   if (!url) {
@@ -30,8 +62,23 @@ exports.handler = (event) => {
     return;
   }
 
+  const idList = getTargetListId(targetList || "DEFAULT");
+  const params = createParams({ url, idList });
+  const response = await createCard(params);
+
+  // something wrong
+  if (!response.ok) {
+    raiseError(`ERR: trello api says response.ok is false ${urlSource}`);
+    return {
+      statusCode: response.status,
+      body: response.statusText,
+    };
+  }
+
+  const cardData = await response.json();
+
   return {
     statusCode: 200,
-    body: "OK boy",
+    body: JSON.stringify({ cardData }),
   };
 };
