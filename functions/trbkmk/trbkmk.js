7fd988e141f39c2a6df1bd9b02df517db0b1beef
Takeshi Takatsudo <takazudo@gmail.com>
2020-06-11 00:57:31 +0900

    trbkmk worked

--- functions/trbkmk/trbkmk.js
+++ functions/trbkmk/trbkmk.js
@@ -1,33 +1,7 @@
 const fetch = require("node-fetch");
 
-const URL = {
-  CREATE_CARD: `${process.env.URL}/.netlify/functions/trello-create-bookmark-card`,
-  UPDATE_CARD_DESC: `${process.env.URL}/.netlify/functions/update-trello-card-desc`,
-  UPDATE_ATTACHED_URL: `${process.env.URL}/.netlify/functions/update-trello-card-attached-url`,
-};
-
 const raiseError = (message) => {
   console.error(message);
-};
-
-const sendCardCreationRequest = async (event) => {
-  const { url, targetList } = JSON.parse(event.body);
-  const response = await fetch(URL.CREATE_CARD, {
-    method: "post",
-    headers: {
-      "x-appsecret": event.headers["x-appsecret"],
-      Accept: "application/json",
-    },
-    body: JSON.stringify({ url, targetList }),
-  });
-  if (!response.ok) {
-    raiseError(`error on card creation`);
-    return {
-      statusCode: response.status,
-      body: response.statusText,
-    };
-  }
-  return await response.json();
 };
 
 exports.handler = async (event) => {
@@ -39,12 +13,70 @@ exports.handler = async (event) => {
     };
   }
 
-  const cardData = await sendCardCreationRequest(event);
+  const envUrl = event.headers["x-debug-env-url"] || process.env.URL;
+  let baseUrl = `${envUrl}/.netlify/functions`;
 
-  console.log(cardData);
+  const URL = {
+    CREATE_CARD: `${baseUrl}/trello-create-bookmark-card`,
+    UPDATE_CARD_DESC: `${baseUrl}/trbkmk-steps-update-card-desc`,
+    EXPAND_ATTACHED_URL: `${baseUrl}/trbkmk-steps-expand-attached-url`,
+  };
 
+  const commonRequestHeaders = {
+    "x-appsecret": event.headers["x-appsecret"],
+    Accept: "application/json",
+  };
+  if (event.headers["x-debug-env-url"]) {
+    commonRequestHeaders["x-debug-env-url"] = envUrl;
+  }
+
+  const sendCardCreationRequest = async (url, targetList) => {
+    const response = await fetch(URL.CREATE_CARD, {
+      method: "post",
+      headers: commonRequestHeaders,
+      body: JSON.stringify({ url, targetList }),
+    });
+    if (!response.ok) {
+      raiseError(`error on card creation`);
+      return {
+        statusCode: response.status,
+        body: response.statusText,
+      };
+    }
+    return await response.json();
+  };
+
+  const updateCardDesc = async (idCard) => {
+    // TODO: handle error
+    //console.log(`idCard: ${idCard}`);
+    const response = await fetch(URL.UPDATE_CARD_DESC, {
+      method: "post",
+      headers: commonRequestHeaders,
+      body: JSON.stringify({ idCard }),
+    });
+    return response.json();
+  };
+
+  const expandAttachedUrl = async (idCard, url) => {
+    // TODO: handle error
+    //console.log(`idCard: ${idCard}`);
+    const response = await fetch(URL.EXPAND_ATTACHED_URL, {
+      method: "post",
+      headers: commonRequestHeaders,
+      body: JSON.stringify({ idCard, url }),
+    });
+    return response.json();
+  };
+
+  const { url, targetList } = JSON.parse(event.body);
+  const { cardData } = await sendCardCreationRequest(url, targetList);
+  const idCard = cardData.id;
+  updateCardDesc(idCard);
+  expandAttachedUrl(idCard, url);
   return {
     statusCode: 400,
-    body: JSON.stringify(cardData),
+    body: JSON.stringify({
+      result: true,
+    }),
   };
 };
