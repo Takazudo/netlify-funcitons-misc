45ae24542675ef91c831876953f42da902ef7fa9
Takeshi Takatsudo <takazudo@NishikiAnago.local>
2020-06-15 03:15:36 +0900

    refactor around sentry

--- functions/utils/index.js
+++ functions/utils/index.js
@@ -3,38 +3,81 @@ const { SENTRY_DSN } = process.env;
 
 let sentryInitialized = false;
 
+const commonSuccessResponse = {
+  statusCode: 200,
+  body: JSON.stringify({ result: true }),
+};
+
+const commonErrorResponse = {
+  statusCode: 400,
+  body: JSON.stringify({ result: false }),
+};
+
+const wait = async (milliseconds) => {
+  return new Promise((resolve) => setTimeout(resolve, milliseconds));
+};
+
+const initSentry = () => {
+  if (!SENTRY_DSN) {
+    return;
+  }
+  Sentry.init({ dsn: SENTRY_DSN });
+  sentryInitialized = true;
+};
+
 const reportError = async (error) => {
   console.warn(error);
   if (!sentryInitialized) return;
-  if (typeof error === 'string') {
-      Sentry.captureMessage(error);
+  if (typeof error === "string") {
+    Sentry.captureMessage(error);
   } else {
-      Sentry.captureException(error);
+    Sentry.captureException(error);
   }
   await Sentry.flush();
-}
+};
+
+const catchErrors = (handler) => {
+  return async function (event, context) {
+    context.callbackWaitsForEmptyEventLoop = false;
+    try {
+      return await handler.call(this, ...arguments);
+    } catch (e) {
+      // This catches both sync errors & promise
+      // rejections, because we 'await' on the handler
+      await reportError(e);
+      return commonErrorResponse;
+    }
+  };
+};
+
+const createBaseUrl = (event) => {
+  const envUrl = createEnvUrl(event);
+  return `${envUrl}/.netlify/functions`;
+};
+
+const createEnvUrl = (event) => {
+  return event.headers["x-debug-env-url"] || process.env.URL;
+};
+
+const createCommonRequestHeaders = (event) => {
+  const envUrl = createEnvUrl(event);
+  const headers = {
+    "x-appsecret": event.headers["x-appsecret"],
+    Accept: "application/json",
+  };
+  if (event.headers["x-debug-env-url"]) {
+    headers["x-debug-env-url"] = envUrl;
+  }
+  return headers;
+};
 
 module.exports = {
-  wait: async (milliseconds) => {
-    return new Promise((resolve) => setTimeout(resolve, milliseconds));
-  },
-  initSentry: () => {
-    if (!SENTRY_DSN) { return; }
-    Sentry.init({ dsn: SENTRY_DSN });
-    sentryInitialized = true;
-  },
-  catchErrors: (handler) => {
-    return async function(event, context) {
-      context.callbackWaitsForEmptyEventLoop = false;
-      try {
-        return await handler.call(this, ...arguments);
-      } catch(e) {
-        // This catches both sync errors & promise
-        // rejections, because we 'await' on the handler
-        await reportError(e);
-        throw e;
-      }
-    };
-  }
-
+  wait,
+  reportError,
+  initSentry,
+  catchErrors,
+  commonErrorResponse,
+  createBaseUrl,
+  createCommonRequestHeaders,
+  commonSuccessResponse,
 };
